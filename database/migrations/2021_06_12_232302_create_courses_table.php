0864428d886fb0663bf4d390fe9178ac995df830
570studio <strausstester@gmail.com>
2021-06-12 19:39:03 -0400

    Add Course and Semester Classes
    Include Class, Factory, Migration and Controllers

---
+++ database/migrations/2021_06_12_232302_create_courses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->text('name')->unique();
+            $table->text('description')->nullable();
+            $table->foreignId('course_id')->references('id')->on('courses')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
