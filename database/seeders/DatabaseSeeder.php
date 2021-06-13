3d812af61dc61f75367e4a4bcdfd04f7080ab61b
570studio <strausstester@gmail.com>
2021-06-13 08:34:05 -0400

    Add Default Sample User with Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            UserSeeder::class,
             SemesterSeeder::class,
             CourseSeeder::class,
         ]);
