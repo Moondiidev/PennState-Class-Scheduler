68ee2754be4bf91ae555c9d2f3053d4d37445340
570studio <strausstester@gmail.com>
2021-06-13 07:25:07 -0400

    Create Pivot Table `course_user`

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Relationships
+     */
+
+    public function courses()
+    {
+        return $this->belongsToMany(Course::class)->as('completed')->withPivot('grade')->withTimestamps();
+    }
+
 }
