3c10a7cbc27406dcb2f43a91ff01dfdbcef2e816
570studio <strausstester@gmail.com>
2021-06-20 08:14:47 -0400

    Add Update Tests

--- tests/Feature/CourseTest.php
+++ tests/Feature/CourseTest.php
@@ -338,5 +338,294 @@ class CourseTest extends TestCase
                                                  "prerequisites" => json_encode([(string) 100])]);
     }
 
+    /**  @test */
+    public function regular_auth_user_can_not_create_a_course()
+    {
+
+        $data = [
+            "title" => "Test Course", "abbreviation" => "Test 442", "description" => "Some random description",
+            "credits" => 2, "semester" => [1,2]
+        ];
+
+        $response = $this->actingAs($this->regularUser)->post(route('courses.store', $data));
+
+        $response->assertForbidden();
+        $this->assertDatabaseMissing('courses', ["title" => "Test Course", "abbreviation" => "Test 442",
+                                             "description" => "Some random description", "credits" => 2,]);
+    }
+
+    /**  @test */
+    public function guest_can_not_create_a_course()
+    {
+
+        $data = [
+            "title" => "Test Course", "abbreviation" => "Test 442", "description" => "Some random description",
+            "credits" => 2, "semester" => [1,2]
+        ];
+
+        $response = $this->post(route('courses.store', $data));
+
+        $response->assertRedirect(route('login'));
+        $this->assertDatabaseMissing('courses', ["title" => "Test Course", "abbreviation" => "Test 442",
+                                                 "description" => "Some random description", "credits" => 2,]);
+    }
+
+
+
+
+
+
+
+
+
+
+
+    /**  @test */
+    public function dev_auth_user_can_update_a_course()
+    {
+
+        $data = [
+            "title" => "Test Course", "abbreviation" => "Test 442", "description" => "Some random description",
+            "credits" => 2, "semester" => [1,2]
+        ];
+
+        $response = $this->actingAs($this->devUser)->from(route('courses.edit', [$this->courseOne->id]))
+            ->put(route('courses.update', [$this->courseOne->id]), $data);
+
+        $response->assertRedirect(route('courses.edit', [$this->courseOne->id]));
+        $this->assertDatabaseHas('courses', ["id" => $this->courseOne->id, "title" => "Test Course",
+                                             "abbreviation" => "Test 442", "description" => "Some random description",
+                                             "credits" => 2,]);
+        $this->assertDatabaseHas('course_semester', ['course_id' => $this->courseOne->id, 'semester_id' => 1]);
+        $this->assertDatabaseHas('course_semester', ['course_id' => $this->courseOne->id, 'semester_id' => 2]);
+    }
+
+    /**  @test */
+    public function dev_auth_user_can_update_a_course_with_prerequisites()
+    {
+
+        $data = [
+            "title" => "Test Course", "abbreviation" => "Test 442", "description" => "Some random description",
+            "credits" => 2, "semester" => [1,2], "prerequisites" => [(string) $this->courseOne->id]
+        ];
+
+        $response = $this->actingAs($this->devUser)->from(route('courses.edit', [$this->courseTwo->id]))
+                         ->put(route('courses.update', [$this->courseTwo->id]), $data);
+
+
+        $response->assertRedirect(route('courses.edit', [$this->courseTwo->id]));
+        $this->assertDatabaseHas('courses', ["id" => $this->courseTwo->id,
+                                             "title" => "Test Course", "abbreviation" => "Test 442",
+                                             "description" => "Some random description", "credits" => 2,
+                                             "prerequisites" => json_encode([(string) $this->courseOne->id])]);
+        $this->assertDatabaseHas('course_semester', ['course_id' => $this->courseTwo->id, 'semester_id' => 1]);
+        $this->assertDatabaseHas('course_semester', ['course_id' => $this->courseTwo->id, 'semester_id' => 2]);
+    }
+
+    /**  @test */
+    public function dev_auth_user_can_update_a_course_with_concurrents()
+    {
+
+        $data = [
+            "title" => "Test Course", "abbreviation" => "Test 442", "description" => "Some random description",
+            "credits" => 2, "semester" => [2], "concurrents" => [(string) $this->courseTwo->id],
+            "prerequisites" => [(string) $this->courseThree->id]
+        ];
+
+        $response = $this->actingAs($this->devUser)->from(route('courses.edit', [$this->courseOne->id]))
+                         ->put(route('courses.update', [$this->courseOne->id]), $data);
+
+
+        $response->assertRedirect(route('courses.edit', [$this->courseOne->id]));
+        $this->assertDatabaseHas('courses', ["title" => "Test Course", "abbreviation" => "Test 442",
+                                             "description" => "Some random description", "credits" => 2,
+                                             "concurrents" => json_encode([(string) $this->courseTwo->id]),
+                                             "prerequisites" => json_encode([(string) $this->courseThree->id])
+        ]);
+
+        $this->assertDatabaseHas('course_semester', ['course_id' => $this->courseOne->id, 'semester_id' => 2]);
+    }
+
+    /**  @test */
+    public function dev_auth_user_can_not_update_a_course_with_a_course_title_already_taken()
+    {
+
+        $data = [
+            "title" => $this->courseTwo->title, "abbreviation" => "Test 442", "description" => "Some random description",
+            "credits" => 2, "semester" => [1,2], "prerequisites" => [(string) $this->courseTwo->id]
+        ];
+
+        $response = $this->actingAs($this->devUser)->from(route('courses.edit', [$this->courseOne->id]))
+                         ->put(route('courses.update', [$this->courseOne->id]), $data);
+
+
+        $response->assertRedirect(route('courses.edit', [$this->courseOne->id]));
+        $response->assertSessionHasErrorsIn('title');
+        $this->assertDatabaseMissing('courses', ["id" => $this->courseOne->id, "title" => $this->courseTwo->title,
+                                                 "abbreviation" => "Test 442",
+                                                 "description" => "Some random description", "credits" => 2,
+                                                 "prerequisites" => json_encode([(string) $this->courseTwo->id])]);
+    }
+
+    /**  @test */
+    public function dev_auth_user_can_not_update_a_course_with_a_course_abbreviation_already_taken()
+    {
+
+        $data = [
+            "title" => "Test Course", "abbreviation" => $this->courseTwo->abbreviation, "description" => "Some random description",
+            "credits" => 2, "semester" => [1,2], "prerequisites" => [(string) $this->courseTwo->id]
+        ];
+
+        $response = $this->actingAs($this->devUser)->from(route('courses.edit', [$this->courseOne->id]))
+                         ->put(route('courses.update', [$this->courseOne->id]), $data);
+
+
+        $response->assertRedirect(route('courses.edit', [$this->courseOne->id]));
+        $response->assertSessionHasErrorsIn('abbreviation');
+        $this->assertDatabaseMissing('courses', ["id" => $this->courseOne->id,"title" => "Test Course",
+                                                 "abbreviation" => $this->courseTwo->abbreviation,
+                                                 "description" => "Some random description", "credits" => 2,
+                                                 "prerequisites" => json_encode([(string) $this->courseTwo->id])]);
+    }
+
+    /**  @test */
+    public function dev_auth_user_can_not_update_a_course_with_no_description()
+    {
+
+        $data = [
+            "title" => "Test Course", "abbreviation" => "Test 442", "description" => "",
+            "credits" => 2, "semester" => [1,2], "prerequisites" => [(string) $this->courseTwo->id]
+        ];
+
+        $response = $this->actingAs($this->devUser)->from(route('courses.edit', [$this->courseOne->id]))
+                         ->put(route('courses.update', [$this->courseOne->id]), $data);
+
+
+        $response->assertRedirect(route('courses.edit', [$this->courseOne->id]));
+        $response->assertSessionHasErrorsIn('description');
+        $this->assertDatabaseMissing('courses', ["id" => $this->courseOne->id, "title" => "Test Course",
+                                                 "abbreviation" => "Test 442", "description" => "", "credits" => 2,
+                                                 "prerequisites" => json_encode([(string) $this->courseTwo->id])]);
+    }
+
+    /**  @test */
+    public function dev_auth_user_can_not_update_a_course_with_invalid_number_of_credits()
+    {
+
+        $data = [
+            "title" => "Test Course", "abbreviation" => "Test 442", "description" => "Some random description",
+            "credits" => 0, "semester" => [1,2], "prerequisites" => [(string) $this->courseTwo->id]
+        ];
+
+        $response = $this->actingAs($this->devUser)->from(route('courses.edit', [$this->courseOne->id]))
+                         ->put(route('courses.update', [$this->courseOne->id]), $data);
+
+
+        $response->assertRedirect(route('courses.edit', [$this->courseOne->id]));
+        $response->assertSessionHasErrorsIn('credits');
+        $this->assertDatabaseMissing('courses', ["id" => $this->courseOne->id,  "title" => "Test Course",
+                                                 "abbreviation" => "Test 442",
+                                                 "description" => "Some random description", "credits" => 0,
+                                                 "prerequisites" => json_encode([(string) $this->courseTwo->id])]);
+    }
+
+    /**  @test */
+    public function dev_auth_user_can_not_update_a_course_with_invalid_semester_choice()
+    {
+
+        $data = [
+            "title" => "Test Course", "abbreviation" => "Test 442", "description" => "Some random description",
+            "credits" => 0, "semester" => [4], "prerequisites" => [(string) $this->courseTwo->id]
+        ];
+
+        $response = $this->actingAs($this->devUser)->from(route('courses.edit', [$this->courseOne->id]))
+                         ->put(route('courses.update', [$this->courseOne->id]), $data);
+
+
+        $response->assertRedirect(route('courses.edit', [$this->courseOne->id]));
+        $response->assertSessionHasErrorsIn('semester');
+        $this->assertDatabaseMissing('courses', ["id" => $this->courseOne->id, "title" => "Test Course",
+                                                 "abbreviation" => "Test 442",
+                                                 "description" => "Some random description", "credits" => 2,
+                                                 "prerequisites" => json_encode([(string) $this->courseTwo->id])]);
+    }
+
+    /**  @test */
+    public function dev_auth_user_can_not_update_a_course_with_invalid_concurrents_choice()
+    {
+
+        $data = [
+            "title" => "Test Course", "abbreviation" => "Test 442", "description" => "Some random description",
+            "credits" => 0, "semester" => [4], "concurrents" => [100]
+        ];
+
+        $response = $this->actingAs($this->devUser)->from(route('courses.edit', [$this->courseOne->id]))
+                         ->put(route('courses.update', [$this->courseOne->id]), $data);
+
+
+        $response->assertRedirect(route('courses.edit', [$this->courseOne->id]));
+        $response->assertSessionHasErrorsIn('concurrents');
+        $this->assertDatabaseMissing('courses', ["id" => $this->courseOne->id, "title" => "Test Course",
+                                                 "abbreviation" => "Test 442",
+                                                 "description" => "Some random description", "credits" => 2,
+                                                 "concurrents" => json_encode([(string) 100])]);
+    }
+
+    /**  @test */
+    public function dev_auth_user_can_not_update_a_course_with_invalid_prerequisites_choice()
+    {
+
+        $data = [
+            "title" => "Test Course", "abbreviation" => "Test 442", "description" => "Some random description",
+            "credits" => 0, "semester" => [4], "prerequisites" => [100]
+        ];
+
+        $response = $this->actingAs($this->devUser)->from(route('courses.edit', [$this->courseOne->id]))
+                         ->put(route('courses.update', [$this->courseOne->id]), $data);
+
+
+        $response->assertRedirect(route('courses.edit', [$this->courseOne->id]));
+        $response->assertSessionHasErrorsIn('prerequisites');
+        $this->assertDatabaseMissing('courses', ["id" => $this->courseOne->id, "title" => "Test Course",
+                                                 "abbreviation" => "Test 442",
+                                                 "description" => "Some random description", "credits" => 2,
+                                                 "prerequisites" => json_encode([(string) 100])]);
+    }
+
+    /**  @test */
+    public function regular_auth_user_can_not_update_a_course()
+    {
+
+        $data = [
+            "title" => "Test Course", "abbreviation" => "Test 442", "description" => "Some random description",
+            "credits" => 2, "semester" => [1,2]
+        ];
+
+        $response = $this->actingAs($this->regularUser)->put(route('courses.update', [$this->courseOne->id]), $data);
+
+        $response->assertForbidden();
+        $this->assertDatabaseMissing('courses', ["id" => $this->courseOne->id, "title" => "Test Course",
+                                                 "abbreviation" => "Test 442",
+                                                 "description" => "Some random description", "credits" => 2,]);
+    }
+
+    /**  @test */
+    public function guest_can_not_update_a_course()
+    {
+
+        $data = [
+            "title" => "Test Course", "abbreviation" => "Test 442", "description" => "Some random description",
+            "credits" => 2, "semester" => [1,2]
+        ];
+
+        $response = $this->put(route('courses.update', [$this->courseOne->id]), $data);
+
+        $response->assertRedirect(route('login'));
+        $this->assertDatabaseMissing('courses', ["id" => $this->courseOne->id, "title" => "Test Course",
+                                                 "abbreviation" => "Test 442",
+                                                 "description" => "Some random description", "credits" => 2,]);
+    }
+
+
 
 }
