da95ae347086a574945fd0067ade6412580d43c5
570studio <strausstester@gmail.com>
2021-07-31 19:02:57 -0400

    Update auth.blade.php

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -32,7 +32,11 @@
 
                     </div>
                 </header>
-                <div class="w-full px-0 py-6">
+                @if(\Illuminate\Support\Facades\Route::current()->getName() == 'courses.index')
+                    <div class="w-full px-0 py-6">
+                @else
+                    <div class="w-full p-6">
+                @endif
 
                     @yield('mainContent')
 
